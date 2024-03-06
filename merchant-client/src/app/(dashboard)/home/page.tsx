9bc6f1a76b23511da558012ea920f09a9dadc600
Mohammad Riyan <mohammadriyan428@gmail.com>
2024-03-07 01:35:56 +0500

    feat: landing page route

---
+++ merchant-client/src/app/(dashboard)/home/page.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@/components/Typography";
+
+export default async function Home() {
+  return (
+    <main className="px-4 pb-10 md:px-12 md:h-[calc(100vh-96px)] flex-center">
+      <Typography variant="title" color="secondary" className="text-center">
+        Dashboard Home
+      </Typography>
+    </main>
+  );
+}
